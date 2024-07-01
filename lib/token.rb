39863f64a2191e5dadb320d00530d90cd4f50b46
John Alvin Ambalong <119690431+jambalong@users.noreply.github.com>
2024-06-30 18:26:03 -0700

    Add Token module

---
+++ lib/token.rb
@@ -0,0 +1,18 @@
+# frozen_string_literal: true
+
+require 'colorize'
+
+# Contains three tokens for game
+module Token
+  def empty_token
+    "○".white
+  end
+
+  def red_token
+    "●".red
+  end
+
+  def blue_token
+    "●".blue
+  end
+end

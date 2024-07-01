dabb5c3bfc630d40fc9483406f98bb8e52d024b1
John Alvin Ambalong <119690431+jambalong@users.noreply.github.com>
2024-06-30 20:02:48 -0700

    Add #display_board

--- lib/board.rb
+++ lib/board.rb
@@ -9,4 +9,11 @@ class Board
   def initialize
     @grid = Array.new(6) { Array.new(7) { empty_token } }
   end
+
+  def display_board
+    @grid.each do |row|
+      puts row.join(' ')
+    end
+    puts (1..7).to_a.join(' ')
+  end
 end

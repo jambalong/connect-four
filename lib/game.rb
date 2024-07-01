a252cf61b02a8f3b6920691443aa6af68d8cac64
John Alvin Ambalong <119690431+jambalong@users.noreply.github.com>
2024-06-30 20:57:08 -0700

    Add #set_players_name

--- lib/game.rb
+++ lib/game.rb
@@ -6,6 +6,8 @@ require_relative 'token'
 
 # Connect Four
 class Game
+  attr_reader :player_one, :player_two
+
   include Token
 
   def initialize
@@ -16,9 +18,21 @@ class Game
 
   def play_game
     display_intro
+    set_players_name
+  end
+
+  def set_players_name
+    player_one.name = get_player_name(1)
+    player_two.name = get_player_name(2)
   end
 
   private
+
+  def get_player_name(player_number)
+    puts "Enter player #{player_number}'s name:"
+    player_name = gets.chomp
+    player_name
+  end
 
   def display_intro
     system 'clear'

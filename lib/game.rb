ef877bb62723c1603ba43e62b99afce0e5f15e32
John Alvin Ambalong <119690431+jambalong@users.noreply.github.com>
2024-07-02 23:42:42 -0700

    Add #run_game_loop

--- lib/game.rb
+++ lib/game.rb
@@ -12,12 +12,13 @@ class Game
     @board = Board.new
     @player_one = Player.new(red_token)
     @player_two = Player.new(blue_token)
+    @turn = 0
   end
 
   def play_game
     display_intro
     set_players_name
-    display_board
+    run_game_loop
   end
 
   def set_players_name
@@ -25,7 +26,56 @@ class Game
     @player_two.name = get_player_name(2)
   end
 
+  def run_game_loop
+    loop do
+      increment_turn
+      display_board
+      player_move(get_current_player)
+      display_board
+
+      break if game_over? || draw?
+    end
+  end
+
   private
+
+  def draw?
+    @turn >= 42
+  end
+
+  def game_over?
+    if @board.winning_move?(@player_one.token)
+      puts "#{@player_one.name} wins!"
+      return true
+    elsif @board.winning_move?(@player_two.token)
+      puts "#{@player_two.name} wins!"
+      return true
+    end
+    false
+  end
+
+  def player_move(player)
+    puts "#{player.name}'s turn (#{player.token})"
+    column = get_valid_column_choice
+    @board.place_token(column, player.token)
+  end
+
+  def get_valid_column_choice
+    loop do
+      puts 'Enter column choice [1-7]:'
+      column = gets.chomp.to_i - 1
+      return column if (0..6).include?(column) && @board.column_not_full?(column)
+      puts 'Invalid column choice. Please choose again.'
+    end
+  end
+
+  def get_current_player
+    @turn.odd? ? @player_one : @player_two
+  end
+
+  def increment_turn
+    @turn += 1
+  end
 
   def display_board
     display_game_title
